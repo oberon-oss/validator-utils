e48a08d5ed53f32623566a426e7a7ec5c9b37afa
TigerLilly64 <info@oberon-oss.eu>
2025-08-26 22:30:53 +0200

    Added javadoc for the PostalCode interface.

--- jakarta/src/main/java/eu/oberon/oss/tools/constraint/annotations/PostalCode.java
+++ jakarta/src/main/java/eu/oberon/oss/tools/constraint/annotations/PostalCode.java
@@ -1,4 +1,7 @@
 package eu.oberon.oss.tools.constraint.annotations;
+
+import eu.oberon.oss.tools.cc.CountryCodeTable;
+import eu.oberon.oss.tools.cc.CountryCodeTableLookupKeys;
 import eu.oberon.oss.tools.constraint.annotations.validators.PostalCodeConstraintValidator;
 import jakarta.validation.Constraint;
 import jakarta.validation.Payload;
@@ -6,13 +9,32 @@ import jakarta.validation.Payload;
 import java.lang.annotation.*;
 
 /**
+ * Validation constraint for Postal codes.
  *
+ * @author TigerLilly64
+ * @since 1.0.0
  */
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.FIELD)
 @Constraint(validatedBy = {PostalCodeConstraintValidator.class})
 public @interface PostalCode {
+
+    /**
+     * Instructs the validator for which country the postal code validation should be performed.
+     * The entry can be identified by the
+     * <ul>
+     * <li>2 letter {@link CountryCodeTableLookupKeys#ISO3166_ALPHA_2 ISO3166_ALPHA_2}</li>
+     * <li>3 letter {@link CountryCodeTableLookupKeys#ISO3166_ALPHA_3 ISO3166_ALPHA_3}</li>
+     * <li>The numeral UNM code {@link CountryCodeTableLookupKeys#UNM49 UNM49}</li>
+     * <li>The exact country name {@link CountryCodeTableLookupKeys#NAME NAME}</li>
+     * </ul>
+     *
+     * @return The country code string specified by the user.
+     * @see CountryCodeTable
+     * @see CountryCodeTableLookupKeys
+     * @since 1.0.0
+     */
     String countryCode() default "??";
 
     /**
